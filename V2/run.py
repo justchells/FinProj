391a3bee4a0842d83c155057afbff9c9c3b76d2c
justchells <justchells@gmail.com>
2014-07-29 08:24:33 +0530

    Template files

--- V2/run.py
+++ V2/run.py
@@ -1,1 +1,51 @@
 #!/usr/bin/python
+
+import regularSip
+import flexStp
+import ma
+import ranked
+import timed
+
+def header(name):
+  print '\n'
+  print '-' * 50
+  print name
+  print '-' * 50
+  print ''
+  
+def main():
+  header('Regular SIP')
+  regularSip.run()
+  
+  header('Flex STP')
+  flexStp.run()
+  
+  header('Regular MA')
+  ma.run('regular', 3)
+  ma.run('regular', 6)
+  ma.run('regular', 9)
+  ma.run('regular', 12)
+  
+  header('Inverted MA')
+  ma.run('inverted', 3)
+  ma.run('inverted', 6)
+  ma.run('inverted', 9)
+  ma.run('inverted', 12)
+  
+  header('Top Ranked')
+  ranked.run('top', 1)
+  ranked.run('top', 2)
+  ranked.run('top', 3)
+  
+  header('Bottom Ranked')
+  ranked.run('bottom', 1)
+  ranked.run('bottom', 2)
+  ranked.run('bottom', 3)
+  
+  header('Timed')
+  timed.run()
+  
+  print '\n'
+  
+if __name__ == '__main__':
+  main()
