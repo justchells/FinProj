ecb150899fc0b26126262f73a7cf232e11fb50ca
justchells <justchells@gmail.com>
2014-07-30 14:59:39 +0530

    MA basic structure

--- V2/ma.py
+++ V2/ma.py
@@ -1,5 +1,26 @@
 #!/usr/bin/python
 
-def run(type, period):
+type = None
+period = None
+
+def set_global_vars(t, p):
+  
+  global type, period
+  type = t
+  period = p
+
+def compute_returns():
   pass
   
+def compute_risk():
+  pass
+  
+def save():
+  pass
+  
+def run(type, period):
+  
+  set_global_vars(type, period)
+  compute_returns()
+  compute_risk()
+  save()
