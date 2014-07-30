2e4ca77e3d03358e4a9f96d227ee489daeee5742
justchells <justchells@gmail.com>
2014-07-30 10:57:55 +0530

    FlexSTP basic structure

--- V2/flexStp.py
+++ V2/flexStp.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python
 
-def run():
+def set_global_vars():
+  pass
+
+def compute_returns():
+  pass
+
+def compute_risk():
+  pass
+
+def save():
   pass
   
+def run():
+  set_global_vars()
+  compute_returns()
+  compute_risk()
+  save()
